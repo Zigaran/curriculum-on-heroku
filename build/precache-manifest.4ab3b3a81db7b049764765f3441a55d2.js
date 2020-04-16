8162bf80c51e1ff0a321ea75e4715d8e84c147e4
Zigaran <zigaraneliasnatan@gmail.com>
2020-04-16 14:37:59 -0300

    Proyecto preparado para realizar deploy en heroku

---
+++ build/precache-manifest.4ab3b3a81db7b049764765f3441a55d2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea24c128b27962e5a4b1f952ebc9d2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7109b1bbb8a8afc94e5d",
+    "url": "./static/css/2.c0e46410.chunk.css"
+  },
+  {
+    "revision": "9201611338ba422a8b38",
+    "url": "./static/css/main.4444a8e2.chunk.css"
+  },
+  {
+    "revision": "7109b1bbb8a8afc94e5d",
+    "url": "./static/js/2.52c616fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.52c616fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9201611338ba422a8b38",
+    "url": "./static/js/main.2a30fdc5.chunk.js"
+  },
+  {
+    "revision": "cc8e5f996e631cfc8a64",
+    "url": "./static/js/runtime-main.b544c700.js"
+  },
+  {
+    "revision": "4a727cff04575e09c7bf2d73b715f2cb",
+    "url": "./static/media/CV-Zigaran.4a727cff.pdf"
+  },
+  {
+    "revision": "caae3d846b6894829f52ae84b3b5742c",
+    "url": "./static/media/ProfileImg.caae3d84.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "./static/media/React.04557c1b.png"
+  },
+  {
+    "revision": "c31831239c0b46e9e5e2d8fd663389d8",
+    "url": "./static/media/SimonSays.c3183123.png"
+  },
+  {
+    "revision": "5ae9636cbcd5fe92ca428c65dc407dd5",
+    "url": "./static/media/Task-Manager.5ae9636c.png"
+  },
+  {
+    "revision": "804c1ac76855cf8a9441380bdbf25ccc",
+    "url": "./static/media/Thailand.804c1ac7.png"
+  },
+  {
+    "revision": "10550ee86b47ea33b21c4df8c6a053e8",
+    "url": "./static/media/css.10550ee8.png"
+  },
+  {
+    "revision": "d1f26ea5ae2f0458bfa69227efd6268c",
+    "url": "./static/media/html.d1f26ea5.png"
+  },
+  {
+    "revision": "03f203769fd5a235846c792ee9e5ecbd",
+    "url": "./static/media/instagram.03f20376.png"
+  },
+  {
+    "revision": "93ab66b05386cdabf908a991a3e5d515",
+    "url": "./static/media/logotipo-de-github.93ab66b0.png"
+  },
+  {
+    "revision": "45e8310ade83c9067d63864d9f8391b1",
+    "url": "./static/media/mapa.45e8310a.png"
+  },
+  {
+    "revision": "34028eead0de8ec04da179b7ee7bd1e9",
+    "url": "./static/media/planeY.34028eea.png"
+  },
+  {
+    "revision": "075750fd533b142cd21c59aae1d86f58",
+    "url": "./static/media/thai-square.075750fd.png"
+  }
+]);
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    USER: 'zigaraneliasnatan',
+    PASS: 'kjjxnvlflxhylmnn'
+}
